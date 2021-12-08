40b9f5213e0aecf7207f7bb27530c6c3e6e6b975
mohamed frindi <mohamedfrindi236@gmail.com>
2021-12-08 12:52:10 +0100

    :hammer: site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
-
 module.exports = {
     entry: './src/doc/index.js',
     mode : 'production',
