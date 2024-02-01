4bec95bae6e8eb56735d748efa869555b0787cf0
phk422 <1769476785@qq.com>
2024-02-01 15:12:50 +0800

    feat: Add default value comments

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,32 @@
 declare module 'txtanime.js' {
   interface TxtAnimeOptions {
-    effect: 'txt-an-1' | 'txt-an-2' | 'txt-an-3' | 'txt-an-4' | 'txt-an-5' | 'txt-an-6' | 'txt-an-7' | 'txt-an-8' | 'txt-an-9',
+    /**
+     * @default 'txt-an-1'
+     */
+    effect?: 'txt-an-1' | 'txt-an-2' | 'txt-an-3' | 'txt-an-4' | 'txt-an-5' | 'txt-an-6' | 'txt-an-7' | 'txt-an-8' | 'txt-an-9',
+    /**
+     * @default 0
+     */
     delayStart?: number,
+    /**
+     * @default 0.07
+     */
     delay?: number,
+    /**
+     * @default 0.7
+     */
     duration?: number,
-    repeat?: number,
+    /**
+     * @default true
+     */
+    repeat?: boolean,
+    /**
+     * @default 2
+     */
     repeatDelay?: number,
+    /**
+     * @default false
+     */
     out?: number | boolean,
     text?: any[],
     overlayBackground?: string
